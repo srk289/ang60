35061c7e07358ae18d5ecb6ccfd90ea945781337
Srikanth Kondaparthy <skondaparthy@daintree.net>
2016-01-21 15:46:29 +1100

    Adds structure

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+          server:{
+            options: {
+              livereload: true,
+              open: {
+
+              }
+            }
+          }
+    },
+    less: {
+      options: {
+        paths: 'assets/',
+        yuicompress: false,
+        ieCompat: true,
+        require: [
+            'css/main.less'
+        ]
+      },
+      src: {
+          expand: true,
+          cwd: 'assets/',
+          src: [
+              'css/*.less'
+          ],
+          ext: '.css',
+          dest: 'assets/'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+            // includes files within path
+            {
+              expand: true, 
+              cwd: 'bower_components/normalize.css/', 
+              src: 'normalize.css',
+              dest: 'assets/css/', 
+              filter: 'isFile'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/jquery/dist/',
+              src: 'jquery.min.js',
+              dest: 'assets/js/vendor/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/jquery-ui/',
+              src: 'jquery-ui.min.js',
+              dest: 'assets/js/vendor/'
+            },
+            {
+              expand: true, 
+              cwd: 'bower_components/jquery-ui/themes/base/', 
+              src: 'resizable.css',
+              dest: 'assets/css/', 
+              filter: 'isFile'
+            }
+          ]
+        }
+    },
+    watch: {
+        css: {
+                files: ['assets/css/*.less', 'assets/css/**/*.less'],
+                tasks: 'less',
+                options: {
+                  livereload: true,
+                }
+            },
+        js: {
+              files: ['assets/js/*.js', 'assets/js/**/*.js'],
+              options: {
+                livereload: true
+              }
+            },
+        views: {
+                files: ['app/views/*.html', 'app/views/**/*.html'],
+                options: {
+                  livereload: true
+                }
+              },
+        html: {
+            files: ['index.html', 'views/**/*'],
+            options: {
+              livereload: true
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  
+  grunt.registerTask('default', ['connect','watch:html']);
+
+};
